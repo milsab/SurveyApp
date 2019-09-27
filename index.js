ee08ea681b5e97965289c2f9194959de60230f1a
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2019-09-26 20:04:09 -0500

    change the message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    return res.send({hi: 'Milad, The Great!!'});
+    return res.send({hi: 'Milad, The Great!! is the King of the Kings!!!'});
 });
 
 const PORT = process.env.PORT || 5000;
