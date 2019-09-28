9c77cc4bffb3aa54e098d9fd155810cf5151100d
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2019-09-28 16:46:59 -0500

    set google oauth

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('/configs/key');
+const keys = require('./config/keys');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('Access Token', accessToken);
+    console.log('Refresh Token', refreshToken);
+    console.log('Profile Token', profile);
 }));
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT); 
