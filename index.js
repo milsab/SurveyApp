693d6ec5dac4dd8fba52d0c1f174c5ffbe155ee9
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2019-09-30 18:37:43 -0500

    setup mongoose

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
-// const authRoutes = require('./routes/authRoutes');
+
 
 const app = express();
+mongoose.connect(keys.mongoURI);
 
+
+// const authRoutes = require('./routes/authRoutes');
 // authRoutes(app);
-
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
