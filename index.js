b3a9cac132f39d407d8a709a4575ccd52544846d
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2019-09-30 17:14:06 -0500

    REfoctore the project structure

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const express = require('express');
+require('./services/passport');
+// const authRoutes = require('./routes/authRoutes');
+
 const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log('Access Token', accessToken);
-    console.log('Refresh Token', refreshToken);
-    console.log('Profile Token', profile);
-}));
+// authRoutes(app);
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
